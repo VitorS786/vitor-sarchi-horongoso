3a52abfa232850d3656891dff34de12d26741a99
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-02-12 20:14:28 -0300

    Adiciona Ex4

---
+++ Mês 02 - Fevereiro/12.02.2026/Scanner/Ex4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+
+		// Criando um objeto Scanner para ler a entrada do usuário
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Informe seu primeiro nome");
+		String nome = input.next();
+
+		System.out.println("Informe o seu sobrenome");
+		String sobrenome = input.next();
+
+		String nomeCompleto = nome + " " + sobrenome;
+
+		System.out.println("Seu nome é: " + nomeCompleto);
+
+	}
+
+}
